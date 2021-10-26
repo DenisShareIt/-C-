29a23cc0dd763059a8d09d66c8890c8bb60d4567
DenisShareIt <kyulyan.denis@gmail.com>
2021-10-26 20:51:26 +0300

    new info for comments

--- 3-C# Examples/Program.cs
+++ 3-C# Examples/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// See https://aka.ms/new-console-template for more informatioin
+// Look for Maximum Value
+// Когда открывается "{" не ставится ";" до момента закрытия "}"
+
+int a = 100;
+int b = 15;
+if(a>b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}

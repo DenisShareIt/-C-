df9ec886c38935e91ef87a8a2f9a74d965e6f236
DenisShareIt <kyulyan.denis@gmail.com>
2021-10-26 20:40:25 +0300

    C to F

---
+++ 2-C#Examples/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+    //How fint what is celsium from farengheit
+
+int C = int.Parse(Console.ReadLine());
+float F = C*1.8f+32;
+Console.WriteLine(F);
